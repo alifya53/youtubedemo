837544a14951613794b7bb72e1ae885093ee0ae9
Alifya53 <alifyahussain53@gmail.com>
2020-05-10 23:42:42 +0500

    new_file

---
+++ live_demo/target/rls/debug/deps/live_demo-26322247406c51e7.d
@@ -0,0 +1,5 @@
+c:\Users\Dell\Desktop\demo1\live_demo\target\rls\debug\deps\live_demo-26322247406c51e7.rmeta: src\main.rs
+
+c:\Users\Dell\Desktop\demo1\live_demo\target\rls\debug\deps\live_demo-26322247406c51e7.d: src\main.rs
+
+src\main.rs:
---
+++ live_demo/target/rls/debug/deps/live_demo-e0b3917c208140ec.d
@@ -0,0 +1,5 @@
+c:\Users\Dell\Desktop\demo1\live_demo\target\rls\debug\deps\live_demo-e0b3917c208140ec.rmeta: src\main.rs
+
+c:\Users\Dell\Desktop\demo1\live_demo\target\rls\debug\deps\live_demo-e0b3917c208140ec.d: src\main.rs
+
+src\main.rs:
